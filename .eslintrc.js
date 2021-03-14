b1a00d25e0f62424e1091ac1484e4e8c149d3123
xiaozhu <809057480@qq.com>
2021-03-14 17:15:44 +0800

    style: 格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': 'off',
-    'space-in-parens': 'off',
-    'no-debugger': 'off',
-    'no-mixed-operators': 'off',
-    'eqeqeq': 'off',
-    'no-unused-vars': 'off',
-    'camelcase': 'off',
-    // allow paren-less arrow functions
-    'arrow-parens': 'off',
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'object-curly-even-spacing': 'on',
-    'one-var': 'off',
-    'indent': 'off',
-    'vue/no-unused-components': 'off',
-    'no-unneeded-ternary': 'off',
-    'semicolon': 'on'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ["plugin:vue/essential", "@vue/standard"],
+    rules: {
+        "no-console": "off",
+        "space-in-parens": "off",
+        "no-debugger": "off",
+        "no-mixed-operators": "off",
+        eqeqeq: "off",
+        "no-unused-vars": "off",
+        camelcase: "off",
+        "arrow-parens": "off",
+        "generator-star-spacing": "off",
+        "object-curly-even-spacing": "on",
+        "one-var": "off",
+        indent: "off",
+        "vue/no-unused-components": "off",
+        "no-unneeded-ternary": "off",
+        semicolon: "on",
+    },
+    parserOptions: {
+        parser: "babel-eslint",
+    },
+};
